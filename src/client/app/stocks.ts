2c31ba30484374cdbf6f8fe7e520bb1a2769c14d
Neo <neolwc@gmail.com>
2016-04-11 18:44:16 -0400

    :rocket:

--- src/client/app/stocks.ts
+++ src/client/app/stocks.ts
@@ -1,16 +1,19 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {CliRouteConfig} from './route-config';
+import {FinanceService} from './yahoo.service';
+import {ListTable} from './list-table/list-table';
 
 @Component({
   selector: 'stocks-app',
-  providers: [ROUTER_PROVIDERS],
+  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS, FinanceService],
   templateUrl: 'app/stocks.html',
   directives: [ROUTER_DIRECTIVES],
   pipes: []
 })
 @RouteConfig([
-
+    { path: '/', name: 'ListTable', component: ListTable }
 ].concat(CliRouteConfig))
 
 export class StocksApp {
