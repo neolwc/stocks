647a66f498a4b63082ea6159b76a89295cf6b3e8
Neo <neolwc@gmail.com>
2016-04-17 16:53:36 -0400

    Test the chart

--- src/client/app/line-chart/line-chart.ts
+++ src/client/app/line-chart/line-chart.ts
@@ -1,14 +1,33 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, OnInit} from 'angular2/core';
 import {FinanceService} from '../yahoo.service';
+
+declare var Chart: any;
 
 @Component({
   selector: 'line-chart',
   templateUrl: 'app//line-chart/line-chart.html',
   styleUrls: ['app//line-chart/line-chart.css']
 })
-export class LineChart {
+export class LineChart implements OnInit {
   @Input() selected;
+  public chart;
+  public data;
 
   constructor(private _financeService: FinanceService) {}
 
+  ngOnInit() {
+    this.data = {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      datasets: [
+        {
+          label: "Average",
+          data: [200*Math.random(),200*Math.random(),200*Math.random(),200*Math.random(),200*Math.random()]
+        }
+      ]
+    }
+    this.chart = new Chart(document.querySelector('canvas'), {
+      type: 'line',
+      data: this.data
+    });
+  }
 }
